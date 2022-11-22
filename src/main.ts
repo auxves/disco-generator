1018861563c810aede2adf1337b7827374417dfc
auxves <me@auxves.dev>
2022-11-22 14:57:52 -0800

    fix exit confirmation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import "./app.scss"
 import App from "./App.svelte"
 
-addEventListener("beforeunload", () =>
-  confirm("Are you sure you want to exit?")
-)
-
 const app = new App({
   target: document.getElementById("app"),
 })
