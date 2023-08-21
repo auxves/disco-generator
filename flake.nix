a71edeb3e3fefbe596a93a69223ac9bd7e081bf9
auxves <me@auxves.dev>
2023-08-21 00:32:07 -0700

    nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "disco-generator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          packages = [ nodejs_20 ];
+        };
+      }
+    );
+}
