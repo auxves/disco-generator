1d0e62a8b3b22a01cc34df3f81fe48688f428fb4
auxves <me@auxves.dev>
2024-05-19 23:59:30 -0700

    add Cross-Origin-Resource-Policy

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,5 +2,6 @@ export function load({ setHeaders }) {
   setHeaders({
     "Cross-Origin-Opener-Policy": "same-origin",
     "Cross-Origin-Embedder-Policy": "require-corp",
+    "Cross-Origin-Resource-Policy": "same-origin",
   })
 }
