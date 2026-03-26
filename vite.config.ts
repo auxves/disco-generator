4ba2f2bde072fe140a6062b69a0998103f2d0052
auxves <me@auxves.dev>
2026-03-26 00:21:56 -0700

    add wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [sveltekit()],
   build: {
-    target: "esnext",
+    target: "es2023",
   },
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
